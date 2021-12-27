b05159680ae81d640e51c85d1d6f72ba572a50f6
Ignacio Freire <nachofreire@gmail.com>
2021-12-27 14:44:31 -0300

    LOSSLESS-815: Removed not needed require on claimCommitteeReward

--- contracts/LosslessGovernance.sol
+++ contracts/LosslessGovernance.sol
@@ -481,7 +481,6 @@ contract LosslessGovernance is Initializable, AccessControlUpgradeable, Pausable
     ///@notice This function is for committee members to claim their rewards
     ///@param reportId report ID to claim reward from
     function claimCommitteeReward(uint256 reportId) public whenNotPaused {
-        require(isReportSolved(reportId), "LSS: Report is not solved");
         require(reportResolution(reportId), "LSS: Report solved negatively");
         require(reportVotes[reportId].committeeMemberVoted[msg.sender], "LSS: Did not vote on report");
 
