607bd1f1bced43d8e38bb1ce454dd1b71594ea34
dom <domantas.pelaitis@gmail.com>
2021-12-27 14:58:03 +0200

    Fix CI/CD

--- hardhat.config.js
+++ hardhat.config.js
@@ -28,33 +28,6 @@ module.exports = {
           },
         },
       },
-      {
-        version: '0.5.16',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.4.18',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
-      {
-        version: '0.6.6',
-        settings: {
-          optimizer: {
-            enabled: true,
-            runs: 200,
-          },
-        },
-      },
     ],
   },
   paths: {
