362d528919fe1e11e7296001f3bb2def0c6d59c8
Ignacio Freire <nachofreire@gmail.com>
2022-01-03 13:46:37 -0300

    LOSSLESS-847: Added event test to proposeNewSettlementPeriod

--- test/domain based tests/proposeSettlementPeriodChanges.js
+++ test/domain based tests/proposeSettlementPeriodChanges.js
@@ -42,6 +42,13 @@ describe(scriptName, () => {
         ).to.not.be.reverted;
       });
 
+      it('should emit event', async () => {
+        await expect(
+          env.lssController.connect(adr.lerc20Admin)
+            .proposeNewSettlementPeriod(lerc20Token.address, 5 * 60),
+        ).to.emit(env.lssController, 'NewSettlementPeriodProposed').withArgs(lerc20Token.address, 5 * 60);
+      });
+
       it('should revert if it\'s not token admin', async () => {
         await expect(
           env.lssController.connect(adr.regularUser1)
