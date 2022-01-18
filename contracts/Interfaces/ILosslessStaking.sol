ecadffe1ef9fc59be69eca9f7ec72a7b1f7a9e13
Ignacio Freire <nachofreire@gmail.com>
2022-01-18 20:19:05 -0300

    CVF-178to189: Various Fixes on Lossless Staking interface

--- contracts/Interfaces/ILosslessStaking.sol
+++ contracts/Interfaces/ILosslessStaking.sol
@@ -1,13 +1,19 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.4;
+pragma solidity ^0.8.0;
+
+import "./ILosslessERC20.sol";
+import "./ILosslessGovernance.sol";
+import "./ILosslessStaking.sol";
+import "./ILosslessReporting.sol";
+import "./ILosslessControllerV3.sol";
 
 interface ILssStaking {
-  function stakingToken() external returns(address);
-  function losslessReporting() external returns(address);
-  function losslessController() external returns(address);
-  function losslessGovernance() external returns(address);
+  function stakingToken() external returns(ILERC20);
+  function losslessReporting() external returns(ILssReporting);
+  function losslessController() external returns(ILssController);
+  function losslessGovernance() external returns(ILssGovernance);
   function stakingAmount() external returns(uint256);
-  function stakers() external returns(address[] memory);
+  function stakers(uint256 reportId) external returns(address[] memory);
   function totalStakedOnReport(uint256 reportId) external returns(uint256);
   function losslessPayed(uint256 reportId) external returns(bool);
   function getVersion() external pure returns (uint256);
@@ -17,15 +23,15 @@ interface ILssStaking {
   
   function pause() external;
   function unpause() external;
-  function setLssReporting(address _losslessReporting) external;
-  function setStakingToken(address _stakingToken) external;
-  function setLosslessGovernance(address _losslessGovernance) external;
+  function setLssReporting(ILssReporting _losslessReporting) external;
+  function setStakingToken(ILERC20 _stakingToken) external;
+  function setLosslessGovernance(ILssGovernance _losslessGovernance) external;
   function setStakingAmount(uint256 _stakingAmount) external;
   function stake(uint256 reportId) external;
   function stakerClaim(uint256 reportId) external;
   function losslessClaim(uint256 reportId) external;
 
-  event Stake(address indexed token, address indexed account, uint256 reportId);
+  event Stake(ILERC20 indexed token, address indexed account, uint256 indexed reportId);
   event StakerClaim(address indexed staker, address indexed token, uint256 indexed reportID);
   event NewStakingAmount(uint256 indexed newAmount);
   event NewStakingToken(address indexed newToken);
