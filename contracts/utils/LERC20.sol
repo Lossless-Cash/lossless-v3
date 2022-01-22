83937186001f52d738dfbc95cd2c5d5c5f101c0e
Ignacio Freire <nachofreire@gmail.com>
2022-01-21 23:18:43 -0300

    Added back checks on _approve to comply with ERC20 standard

--- contracts/utils/LERC20.sol
+++ contracts/utils/LERC20.sol
@@ -214,6 +214,9 @@ contract LERC20 is Context, ILERC20 {
     }
 
     function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
         _allowances[owner][spender] = amount;
         emit Approval(owner, spender, amount);
     }
