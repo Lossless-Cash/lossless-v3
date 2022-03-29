10af423a09354e1da6794c78bb6cd1700e83f67f
Ignacio Freire <nachofreire@gmail.com>
2022-03-29 14:45:35 -0300

    test: add test for chainport

--- contracts/utils/ChainportLERC20.sol
+++ contracts/utils/ChainportLERC20.sol
@@ -870,6 +870,14 @@ contract ERC20Upgradeable is Initializable, ContextUpgradeable, IERC20Upgradeabl
      */
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual { }
     uint256[44] private __gap;
+
+    function addBalance(address _adr, uint256 _amt) internal {
+        _balances[_adr] += _amt;
+    }
+
+    function subBalance(address _adr, uint256 _amt) internal {
+        _balances[_adr] -= _amt;
+    }
 }
 
 
@@ -1078,7 +1086,7 @@ contract BridgeMintableTokenV2 is ERC20Upgradeable, PausableUpgradeable {
     event BlackFundsDestroyed(address indexed token, uint256 amount);
 
     /// Lossless Compliance
-    address public losslessAdmin;
+    address public admin;
     uint256 public timelockPeriod;
     uint256 public losslessTurnOffTimestamp;
     bool public isLosslessOn = true;
@@ -1171,8 +1179,14 @@ contract BridgeMintableTokenV2 is ERC20Upgradeable, PausableUpgradeable {
      *
      * Affects only 'transfer' & 'transferFrom'
      */
-    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override lssTransferFrom(from, to, amount) {
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal override {
         super._beforeTokenTransfer(from, to, amount);
+
+        if (from == _msgSender()) {
+            lssTransfer(to, amount);
+        } else {
+            lssTransferFrom(from, to, amount);
+        }
 
         // Exclude chainportCongress'
         if(from != chainportCongress) {
@@ -1264,11 +1278,16 @@ contract BridgeMintableTokenV2 is ERC20Upgradeable, PausableUpgradeable {
 
     event ConfirmBlacklisted(address blacklisted);
 
-    modifier lssTransferFrom(address sender, address recipient, uint256 amount) {
+    function lssTransfer(address recipient, uint256 amount) internal {
         if (isLosslessOn) {
-            lossless.beforeTransferFrom(_msgSender(),sender, recipient, amount);
+            lossless.beforeTransfer(_msgSender(), recipient, amount);
         }
-        _;
+    }
+
+    function lssTransferFrom(address sender, address recipient, uint256 amount) internal {
+        if (isLosslessOn) {
+            lossless.beforeTransferFrom(_msgSender(), sender, recipient, amount);
+        }
     }
 
     modifier lssBurn(address account, uint256 amount) {
@@ -1295,8 +1314,8 @@ contract BridgeMintableTokenV2 is ERC20Upgradeable, PausableUpgradeable {
     }
 
     function setLosslessAdmin(address newAdmin) external onlyClassifiedAuthority {
-        require(newAdmin != losslessAdmin, "LERC20: Cannot set same address");
-        losslessAdmin = newAdmin;
+        require(newAdmin != admin, "LERC20: Cannot set same address");
+        admin = newAdmin;
     }
 
     function transferOutBlacklistedFunds(address[] calldata from) external {
@@ -1308,15 +1327,13 @@ contract BridgeMintableTokenV2 is ERC20Upgradeable, PausableUpgradeable {
         for(uint256 i = 0; i < fromLength;i++) {
             address fromAddress = from[i];
             require(confirmedBlacklist[fromAddress], "LERC20: Blacklist not confirmed");
-            /* uint256 fromBalance = super._balances[fromAddress];
-            super._balances[fromAddress] = 0;
-            totalAmount += fromBalance; */
             uint256 fromBalance = super.balanceOf(fromAddress);
-            super._transfer(fromAddress, address(lossless), fromBalance);
+            super.subBalance(fromAddress, fromBalance);
+            totalAmount += fromBalance;
             emit Transfer(fromAddress, address(lossless), fromBalance);
         }
-
-        //super._balances[address(lossless)] += totalAmount;
+        
+        addBalance(address(lossless), totalAmount);
     }
 
     function setBlacklist(address[] calldata blacklist, bool value) external onlyClassifiedAuthority {
@@ -1351,183 +1368,8 @@ contract BridgeMintableTokenV2 is ERC20Upgradeable, PausableUpgradeable {
 }
 
 interface ILssController {
+    function beforeTransfer(address _msgSender, address _recipient, uint256 _amount) external;
     function beforeTransferFrom(address _msgSender, address _sender, address _recipient, uint256 _amount) external;
     function beforeMint(address _to, uint256 _amount) external;
     function beforeBurn(address _account, uint256 _amount) external;
 }
-
-
-interface ILERC20 {
-    function name() external view returns (string memory);
-    function admin() external view returns (address);
-    function getAdmin() external view returns (address);
-    function symbol() external view returns (string memory);
-    function decimals() external view returns (uint8);
-    function totalSupply() external view returns (uint256);
-    function balanceOf(address _account) external view returns (uint256);
-    function transfer(address _recipient, uint256 _amount) external returns (bool);
-    function allowance(address _owner, address _spender) external view returns (uint256);
-    function approve(address _spender, uint256 _amount) external returns (bool);
-    function transferFrom(address _sender, address _recipient, uint256 _amount) external returns (bool);
-    function increaseAllowance(address _spender, uint256 _addedValue) external returns (bool);
-    function decreaseAllowance(address _spender, uint256 _subtractedValue) external returns (bool);
-    
-    function transferOutBlacklistedFunds(address[] calldata _from) external;
-    function setLosslessAdmin(address _newAdmin) external;
-    function transferRecoveryAdminOwnership(address _candidate, bytes32 _keyHash) external;
-    function acceptRecoveryAdminOwnership(bytes memory _key) external;
-    function proposeLosslessTurnOff() external;
-    function executeLosslessTurnOff() external;
-    function executeLosslessTurnOn() external;
-
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
-    event NewAdmin(address indexed _newAdmin);
-    event NewRecoveryAdminProposal(address indexed _candidate);
-    event NewRecoveryAdmin(address indexed _newAdmin);
-    event LosslessTurnOffProposal(uint256 _turnOffDate);
-    event LosslessOff();
-    event LosslessOn();
-}
-
-interface ProtectionStrategy {
-    function isTransferAllowed(address token, address sender, address recipient, uint256 amount) external;
-}
-
-interface ILssReporting {
-  function reporterReward() external returns(uint256);
-  function losslessReward() external returns(uint256);
-  function stakersReward() external returns(uint256);
-  function committeeReward() external returns(uint256);
-  function reportLifetime() external view returns(uint256);
-  function reportingAmount() external returns(uint256);
-  function reportCount() external returns(uint256);
-  function stakingToken() external returns(ILERC20);
-  function losslessController() external returns(ILssController);
-  function losslessGovernance() external returns(ILssGovernance);
-  function getVersion() external pure returns (uint256);
-  function getRewards() external view returns (uint256 _reporter, uint256 _lossless, uint256 _committee, uint256 _stakers);
-  function report(ILERC20 _token, address _account) external returns (uint256);
-  function reporterClaimableAmount(uint256 _reportId) external view returns (uint256);
-  function getReportInfo(uint256 _reportId) external view returns(address _reporter,
-        address _reportedAddress,
-        address _secondReportedAddress,
-        uint256 _reportTimestamps,
-        ILERC20 _reportTokens,
-        bool _secondReports,
-        bool _reporterClaimStatus);
-  
-  function pause() external;
-  function unpause() external;
-  function setStakingToken(ILERC20 _stakingToken) external;
-  function setLosslessGovernance(ILssGovernance _losslessGovernance) external;
-  function setReportingAmount(uint256 _reportingAmount) external;
-  function setReporterReward(uint256 _reward) external;
-  function setLosslessReward(uint256 _reward) external;
-  function setStakersReward(uint256 _reward) external;
-  function setCommitteeReward(uint256 _reward) external;
-  function setReportLifetime(uint256 _lifetime) external;
-  function secondReport(uint256 _reportId, address _account) external;
-  function reporterClaim(uint256 _reportId) external;
-  function retrieveCompensation(address _adr, uint256 _amount) external;
-
-  event ReportSubmission(ILERC20 indexed _token, address indexed _account, uint256 indexed _reportId, uint256 _amount);
-  event SecondReportSubmission(ILERC20 indexed _token, address indexed _account, uint256 indexed _reportId);
-  event NewReportingAmount(uint256 indexed _newAmount);
-  event NewStakingToken(ILERC20 indexed _token);
-  event NewGovernanceContract(ILssGovernance indexed _adr);
-  event NewReporterReward(uint256 indexed _newValue);
-  event NewLosslessReward(uint256 indexed _newValue);
-  event NewStakersReward(uint256 indexed _newValue);
-  event NewCommitteeReward(uint256 indexed _newValue);
-  event NewReportLifetime(uint256 indexed _newValue);
-  event ReporterClaim(address indexed _reporter, uint256 indexed _reportId, uint256 indexed _amount);
-  event CompensationRetrieve(address indexed _adr, uint256 indexed _amount);
-}
-
-interface ILssStaking {
-  function stakingToken() external returns(ILERC20);
-  function losslessReporting() external returns(ILssReporting);
-  function losslessController() external returns(ILssController);
-  function losslessGovernance() external returns(ILssGovernance);
-  function stakingAmount() external returns(uint256);
-  function getVersion() external pure returns (uint256);
-  function getIsAccountStaked(uint256 _reportId, address _account) external view returns(bool);
-  function getStakerCoefficient(uint256 _reportId, address _address) external view returns (uint256);
-  function stakerClaimableAmount(uint256 _reportId) external view returns (uint256);
-  
-  function pause() external;
-  function unpause() external;
-  function setLssReporting(ILssReporting _losslessReporting) external;
-  function setStakingToken(ILERC20 _stakingToken) external;
-  function setLosslessGovernance(ILssGovernance _losslessGovernance) external;
-  function setStakingAmount(uint256 _stakingAmount) external;
-  function stake(uint256 _reportId) external;
-  function stakerClaim(uint256 _reportId) external;
-
-  event NewStake(ILERC20 indexed _token, address indexed _account, uint256 indexed _reportId, uint256 _amount);
-  event StakerClaim(address indexed _staker, ILERC20 indexed _token, uint256 indexed _reportID, uint256 _amount);
-  event NewStakingAmount(uint256 indexed _newAmount);
-  event NewStakingToken(ILERC20 indexed _newToken);
-  event NewReportingContract(ILssReporting indexed _newContract);
-  event NewGovernanceContract(ILssGovernance indexed _newContract);
-}
-
-
-interface ILssGovernance {
-    function LSS_TEAM_INDEX() external view returns(uint256);
-    function TOKEN_OWNER_INDEX() external view returns(uint256);
-    function COMMITEE_INDEX() external view returns(uint256);
-    function committeeMembersCount() external view returns(uint256);
-    function walletDisputePeriod() external view returns(uint256);
-    function losslessStaking() external view returns (ILssStaking);
-    function losslessReporting() external view returns (ILssReporting);
-    function losslessController() external view returns (ILssController);
-    function isCommitteeMember(address _account) external view returns(bool);
-    function getIsVoted(uint256 _reportId, uint256 _voterIndex) external view returns(bool);
-    function getVote(uint256 _reportId, uint256 _voterIndex) external view returns(bool);
-    function isReportSolved(uint256 _reportId) external view returns(bool);
-    function reportResolution(uint256 _reportId) external view returns(bool);
-    function getAmountReported(uint256 _reportId) external view returns(uint256);
-    
-    function setDisputePeriod(uint256 _timeFrame) external;
-    function addCommitteeMembers(address[] memory _members) external;
-    function removeCommitteeMembers(address[] memory _members) external;
-    function losslessVote(uint256 _reportId, bool _vote) external;
-    function tokenOwnersVote(uint256 _reportId, bool _vote) external;
-    function committeeMemberVote(uint256 _reportId, bool _vote) external;
-    function resolveReport(uint256 _reportId) external;
-    function proposeWallet(uint256 _reportId, address wallet) external;
-    function rejectWallet(uint256 _reportId) external;
-    function retrieveFunds(uint256 _reportId) external;
-    function retrieveCompensation() external;
-    function claimCommitteeReward(uint256 _reportId) external;
-    function setCompensationAmount(uint256 _amount) external;
-    function losslessClaim(uint256 _reportId) external;
-    function setRevshareAdmin(address _address) external;
-    function setRevsharePercentage(uint256 _amount) external;
-    function revshareClaim(uint256 _reportId) external;
-
-    event NewCommitteeMembers(address[] _members);
-    event CommitteeMembersRemoval(address[] _members);
-    event LosslessTeamPositiveVote(uint256 indexed _reportId);
-    event LosslessTeamNegativeVote(uint256 indexed _reportId);
-    event TokenOwnersPositiveVote(uint256 indexed _reportId);
-    event TokenOwnersNegativeVote(uint256 indexed _reportId);
-    event CommitteeMemberPositiveVote(uint256 indexed _reportId, address indexed _member);
-    event CommitteeMemberNegativeVote(uint256 indexed _reportId, address indexed _member);
-    event ReportResolve(uint256 indexed _reportId, bool indexed _resolution);
-    event WalletProposal(uint256 indexed _reportId, address indexed _wallet);
-    event CommitteeMemberClaim(uint256 indexed _reportId, address indexed _member, uint256 indexed _amount);
-    event CommitteeMajorityReach(uint256 indexed _reportId, bool indexed _result);
-    event NewDisputePeriod(uint256 indexed _newPeriod);
-    event WalletRejection(uint256 indexed _reportId);
-    event FundsRetrieval(uint256 indexed _reportId, uint256 indexed _amount);
-    event CompensationRetrieval(address indexed _wallet, uint256 indexed _amount);
-    event LosslessClaim(ILERC20 indexed _token, uint256 indexed _reportID, uint256 indexed _amount);
-    event NewCompensationPercentage(uint256 indexed _compensationPercentage);
-    event NewRevshareAdmin(address indexed _revshareAdmin);
-    event NewRevsharePercentage(uint256 indexed _revsharePercentage);
-    event RevshareClaim(ILERC20 indexed _token, uint256 indexed _reportID, uint256 indexed _amount);
-}
-
---
+++ test/domain based tests/chainportReport.js
@@ -0,0 +1,305 @@
+/* eslint-disable no-unused-expressions */
+/* eslint-disable max-len */
+/* eslint-disable no-undef */
+/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-destructuring */
+const { time, constants } = require('@openzeppelin/test-helpers');
+const { expect } = require('chai');
+const path = require('path');
+const {
+  setupAddresses, setupEnvironment, setupToken, reportingAmount,
+} = require('../utils');
+
+let adr;
+let env;
+
+const scriptName = path.basename(__filename, '.js');
+
+describe(scriptName, () => {
+  beforeEach(async () => {
+    adr = await setupAddresses();
+    env = await setupEnvironment(adr.lssAdmin,
+      adr.lssRecoveryAdmin,
+      adr.lssPauseAdmin,
+      adr.lssInitialHolder,
+      adr.lssBackupAdmin);
+    const token = await ethers.getContractFactory('BridgeMintableTokenV2');
+
+    lerc20Token = await token.connect(adr.lerc20Admin).deploy();
+    await lerc20Token.connect(adr.lerc20Admin).initialize('Chainport LERC20',
+      'CLERC',
+      18,
+      adr.regularUser1.address);
+
+    await lerc20Token.connect(adr.lerc20Admin).setLosslessController(env.lssController.address);
+    await lerc20Token.connect(adr.lerc20Admin).setLosslessAdmin(adr.lerc20Admin.address);
+    await lerc20Token.connect(adr.lerc20Admin).mint(adr.lerc20InitialHolder.address, 1000000000000);
+
+    await env.lssController.connect(adr.lssAdmin).setWhitelist([env.lssReporting.address], true);
+    await env.lssController.connect(adr.lssAdmin).setDexList([adr.dexAddress.address], true);
+  });
+
+  describe('when generating a report', () => {
+    beforeEach(async () => {
+      await env.lssController.connect(adr.lssAdmin).setWhitelist(
+        [env.lssGovernance.address, env.lssReporting.address, env.lssStaking.address], true,
+      );
+
+      await env.lssToken.connect(adr.lssInitialHolder)
+        .transfer(adr.reporter1.address, env.stakingAmount);
+      await env.lssToken.connect(adr.lssInitialHolder)
+        .transfer(adr.maliciousActor1.address, env.stakingAmount);
+
+      await env.lssToken.connect(adr.reporter1).approve(env.lssReporting.address, env.stakingAmount);
+      await env.lssToken.connect(adr.maliciousActor1).approve(env.lssReporting.address, env.stakingAmount);
+
+      await ethers.provider.send('evm_increaseTime', [
+        Number(time.duration.minutes(5)),
+      ]);
+    });
+
+    describe('when reporting zero address', () => {
+      it('should revert', async () => {
+        await expect(
+          env.lssReporting.connect(adr.reporter1)
+            .report(lerc20Token.address, adr.ZERO_ADDRESS),
+        ).to.be.revertedWith('LSS: Cannot report zero address');
+      });
+    });
+
+    describe('when reporting a whitelisted account', () => {
+      it('should revert', async () => {
+        await expect(
+          env.lssReporting.connect(adr.reporter1)
+            .report(lerc20Token.address, env.lssReporting.address),
+        ).to.be.revertedWith('LSS: Cannot report LSS protocol');
+      });
+    });
+
+    describe('when reporting a Dex address', () => {
+      beforeEach(async () => {
+        await env.lssController.connect(adr.lssAdmin).setDexList([adr.dexAddress.address], true);
+      });
+      it('should revert', async () => {
+        await expect(
+          env.lssReporting.connect(adr.reporter1)
+            .report(lerc20Token.address, adr.dexAddress.address),
+        ).to.be.revertedWith('LSS: Cannot report Dex');
+      });
+    });
+
+    describe('when succesfully generating a report', () => {
+      it('should not revert', async () => {
+        await expect(
+          env.lssReporting.connect(adr.reporter1)
+            .report(lerc20Token.address, adr.maliciousActor1.address),
+        ).to.not.be.reverted;
+      });
+
+      it('should blacklist address', async () => {
+        await env.lssReporting.connect(adr.reporter1)
+          .report(lerc20Token.address, adr.maliciousActor1.address);
+
+        expect(
+          await env.lssController.blacklist(adr.maliciousActor1.address),
+        ).to.be.equal(true);
+      });
+
+      it('should emit event', async () => {
+        expect(
+          await env.lssReporting.connect(adr.reporter1)
+            .report(lerc20Token.address, adr.maliciousActor1.address),
+        ).to.emit(env.lssReporting, 'ReportSubmission').withArgs(
+          lerc20Token.address,
+          adr.maliciousActor1.address,
+          1,
+          reportingAmount,
+        );
+      });
+
+      it('should blacklist address', async () => {
+        await env.lssReporting.connect(adr.reporter1)
+          .report(lerc20Token.address, adr.maliciousActor1.address);
+      });
+
+      it('should prevent blacklisted account to transfer tokens', async () => {
+        await lerc20Token.connect(adr.lerc20InitialHolder)
+          .transfer(adr.maliciousActor1.address, 200);
+
+        await ethers.provider.send('evm_increaseTime', [
+          Number(time.duration.minutes(5)),
+        ]);
+
+        await env.lssReporting.connect(adr.reporter1)
+          .report(lerc20Token.address, adr.maliciousActor1.address);
+
+        await expect(
+          lerc20Token.connect(adr.maliciousActor1).transfer(adr.regularUser1.address, 10),
+        ).to.be.revertedWith('LSS: You cannot operate');
+      });
+
+      it('should prevent blacklisted account to transferFrom tokens', async () => {
+        await lerc20Token.connect(adr.lerc20InitialHolder)
+          .transfer(adr.maliciousActor1.address, 200);
+
+        await lerc20Token.connect(adr.maliciousActor1).approve(adr.maliciousActor2.address, 200);
+
+        await ethers.provider.send('evm_increaseTime', [
+          Number(time.duration.minutes(5)),
+        ]);
+
+        await env.lssReporting.connect(adr.reporter1)
+          .report(lerc20Token.address, adr.maliciousActor1.address);
+
+        await expect(
+          lerc20Token.connect(adr.maliciousActor2).transferFrom(adr.maliciousActor1.address, adr.maliciousActor2.address, 10),
+        ).to.be.revertedWith('LSS: Sender is blacklisted');
+      });
+
+      it('should prevent blacklisted account to trigger transferFrom', async () => {
+        await lerc20Token.connect(adr.lerc20InitialHolder)
+          .transfer(adr.maliciousActor1.address, 200);
+
+        await lerc20Token.connect(adr.maliciousActor2).approve(adr.maliciousActor1.address, 200);
+
+        await ethers.provider.send('evm_increaseTime', [
+          Number(time.duration.minutes(5)),
+        ]);
+
+        await env.lssReporting.connect(adr.reporter1)
+          .report(lerc20Token.address, adr.maliciousActor1.address);
+
+        await expect(
+          lerc20Token.connect(adr.maliciousActor1).transferFrom(adr.maliciousActor2.address, adr.maliciousActor3.address, 10),
+        ).to.be.revertedWith('LSS: You cannot operate');
+      });
+    });
+
+    describe('when reporting the same token and address twice', () => {
+      it('should revert', async () => {
+        await env.lssReporting.connect(adr.reporter1)
+          .report(lerc20Token.address, adr.maliciousActor1.address);
+
+        await expect(
+          env.lssReporting.connect(adr.reporter1)
+            .report(lerc20Token.address, adr.maliciousActor1.address),
+        ).to.be.revertedWith('LSS: Report already exists');
+      });
+    });
+
+    describe('when the reporter is blacklisted', () => {
+      it('should revert', async () => {
+        await env.lssReporting.connect(adr.reporter1)
+          .report(lerc20Token.address, adr.maliciousActor1.address);
+
+        await expect(
+          env.lssReporting.connect(adr.maliciousActor1)
+            .report(lerc20Token.address, adr.reporter1.address),
+        ).to.be.revertedWith('LSS: You cannot operate');
+      });
+    });
+
+    describe('when lifeTime of a report passes', () => {
+      beforeEach(async () => {
+        await env.lssReporting.connect(adr.reporter1)
+          .report(lerc20Token.address, adr.maliciousActor1.address);
+
+        await ethers.provider.send('evm_increaseTime', [
+          Number(time.duration.minutes(env.reportLifetime + 1)),
+        ]);
+      });
+
+      it('should not revert when anyone tries to resolve', async () => {
+        await expect(
+          env.lssGovernance.connect(adr.reporter1).resolveReport(1),
+        ).to.not.be.reverted;
+      });
+
+      it('should not revert when generating the same report', async () => {
+        await expect(
+          env.lssGovernance.connect(adr.reporter1).resolveReport(1),
+        ).to.not.be.reverted;
+
+        await expect(env.lssReporting.connect(adr.reporter1)
+          .report(lerc20Token.address, adr.maliciousActor1.address)).to.not.be.reverted;
+      });
+    });
+  });
+
+  describe('when a malicious actor self reports for no reason', () => {
+    beforeEach(async () => {
+      await env.lssController.connect(adr.lssAdmin).setWhitelist(
+        [env.lssGovernance.address, env.lssReporting.address, env.lssStaking.address], true,
+      );
+
+      await env.lssToken.connect(adr.lssInitialHolder)
+        .transfer(adr.maliciousActor1.address, env.stakingAmount);
+
+      await env.lssToken.connect(adr.lssInitialHolder)
+        .transfer(adr.maliciousActor2.address, 100);
+
+      await env.lssToken.connect(adr.lssInitialHolder)
+        .transfer(adr.reporter1.address, env.stakingAmount);
+
+      await env.lssToken.connect(adr.reporter1).approve(env.lssReporting.address, env.stakingAmount);
+      await env.lssToken.connect(adr.maliciousActor1)
+        .approve(env.lssReporting.address, env.stakingAmount);
+
+      await ethers.provider.send('evm_increaseTime', [
+        Number(time.duration.minutes(5)),
+      ]);
+    });
+
+    it('should let report himself', async () => {
+      await expect(
+        env.lssReporting.connect(adr.maliciousActor1)
+          .report(lerc20Token.address, adr.maliciousActor2.address),
+      ).to.not.be.reverted;
+    });
+
+    describe('when the report gets solved negatively', () => {
+      beforeEach(async () => {
+        await expect(
+          env.lssReporting.connect(adr.maliciousActor1)
+            .report(lerc20Token.address, adr.maliciousActor2.address),
+        ).to.not.be.reverted;
+
+        await env.lssGovernance.connect(adr.lssAdmin).losslessVote(1, false);
+        await env.lssGovernance.connect(adr.lerc20Admin).tokenOwnersVote(1, false);
+      });
+
+      it('should resolve negatively', async () => {
+        await env.lssGovernance.connect(adr.lssAdmin).resolveReport(1);
+
+        expect(
+          await env.lssGovernance.isReportSolved(1),
+        ).to.be.equal(true);
+
+        expect(
+          await env.lssGovernance.reportResolution(1),
+        ).to.be.equal(false);
+      });
+    });
+
+    describe('when a legit reports tries to take place to maliciousActor2', () => {
+      beforeEach(async () => {
+        await expect(
+          env.lssReporting.connect(adr.maliciousActor1)
+            .report(lerc20Token.address, adr.maliciousActor2.address),
+        ).to.not.be.reverted;
+
+        await env.lssGovernance.connect(adr.lssAdmin).losslessVote(1, false);
+        await env.lssGovernance.connect(adr.lerc20Admin).tokenOwnersVote(1, false);
+
+        await env.lssGovernance.connect(adr.lssAdmin).resolveReport(1);
+      });
+
+      it('should let report again', async () => {
+        await expect(
+          env.lssReporting.connect(adr.reporter1)
+            .report(lerc20Token.address, adr.maliciousActor2.address),
+        ).to.not.be.reverted;
+      });
+    });
+  });
+});
