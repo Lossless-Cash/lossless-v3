8875866eb907fcdfc95b7306b17c9175f030ec49
domantasp <domantas.pelaitis@gmail.com>
2022-04-12 12:55:49 +0300

    fix: pipeline

--- hardhat.config.js
+++ hardhat.config.js
@@ -66,64 +66,64 @@ module.exports = {
     artifacts: './src/artifacts',
   },
 
-  networks: {
-    // --- TESTNETS ---
-    ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/YzORPoYzWj2GrWSEmJrelCNuw5FsziK6`,
-      accounts: [PRIVATE_KEY],
-    },
-    rinkeby: {
-      url: 'https://eth-rinkeby.alchemyapi.io/v2/QmxZu6QhVDaiBxWro1HHkTJNNgE-4Ga_',
-      accounts: [PRIVATE_KEY],
-    },
-    bsctest: {
-      url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
-      accounts: [PRIVATE_KEY],
-    },
-    matic_test: {
-      url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/polygon/mumbai/archive`,
-      accounts: [PRIVATE_KEY],
-    },
-    avax_test: {
-      url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/avalanche/testnet`,
-      accounts: [PRIVATE_KEY],
-    },
-    fantom_test: {
-      url: 'https://rpc.testnet.fantom.network/',
-      accounts: [PRIVATE_KEY],
-    },
-    harmony_test: {
-      url: 'https://api.s0.b.hmny.io/',
-      accounts: [PRIVATE_KEY],
-    },
+  // networks: {
+  //   // --- TESTNETS ---
+  //   ropsten: {
+  //     url: `https://eth-ropsten.alchemyapi.io/v2/YzORPoYzWj2GrWSEmJrelCNuw5FsziK6`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   rinkeby: {
+  //     url: 'https://eth-rinkeby.alchemyapi.io/v2/QmxZu6QhVDaiBxWro1HHkTJNNgE-4Ga_',
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   bsctest: {
+  //     url: `https://data-seed-prebsc-1-s1.binance.org:8545/`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   matic_test: {
+  //     url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/polygon/mumbai/archive`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   avax_test: {
+  //     url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/avalanche/testnet`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   fantom_test: {
+  //     url: 'https://rpc.testnet.fantom.network/',
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   harmony_test: {
+  //     url: 'https://api.s0.b.hmny.io/',
+  //     accounts: [PRIVATE_KEY],
+  //   },
 
-    // --- MAINNETS ---
+  //   // --- MAINNETS ---
 
-    eth: {
-      url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/eth/mainnet/archive`,
-      accounts: [PRIVATE_KEY],
-    },
-    bsc: {
-      url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/bsc/mainnet/archive`,
-      accounts: [PRIVATE_KEY],
-    },
-    matic: {
-      url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/polygon/mainnet/archive`,
-      accounts: [PRIVATE_KEY],
-    },
-    avax: {
-      url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/avalanche/mainnet`,
-      accounts: [PRIVATE_KEY],
-    },
-    fantom: {
-      url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/fantom/mainnet`,
-      accounts: [PRIVATE_KEY],
-    },
-    harmony: {
-      url: 'https://api.harmony.one/',
-      accounts: [PRIVATE_KEY],
-    },
-  },
+  //   eth: {
+  //     url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/eth/mainnet/archive`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   bsc: {
+  //     url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/bsc/mainnet/archive`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   matic: {
+  //     url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/polygon/mainnet/archive`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   avax: {
+  //     url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/avalanche/mainnet`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   fantom: {
+  //     url: `https://speedy-nodes-nyc.moralis.io/${MORALIS_KEY}/fantom/mainnet`,
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  //   harmony: {
+  //     url: 'https://api.harmony.one/',
+  //     accounts: [PRIVATE_KEY],
+  //   },
+  // },
   // gasReporter: {
   //   currency: 'USD',
   //   gasPrice: 70,
